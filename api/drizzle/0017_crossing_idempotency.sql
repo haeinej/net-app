3eb9d63f43dc5480f9231612317a63ef6edc25b0
haeinej <haeinej@gmail.com>
2026-03-16 07:58:25 -0700

    Fix crossing idempotency migration syntax

--- api/drizzle/0017_crossing_idempotency.sql
+++ api/drizzle/0017_crossing_idempotency.sql
@@ -1,21 +1,12 @@
 ALTER TABLE "crossings"
   ADD COLUMN IF NOT EXISTS "source_draft_id" uuid;
 
-DO $$
-BEGIN
-  IF NOT EXISTS (
-    SELECT 1
-    FROM pg_constraint
-    WHERE conname = 'crossings_source_draft_fk'
-  ) THEN
-    ALTER TABLE "crossings"
-      ADD CONSTRAINT "crossings_source_draft_fk"
-      FOREIGN KEY ("source_draft_id")
-      REFERENCES "crossing_drafts"("id")
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION;
-  END IF;
-END $$;
+ALTER TABLE "crossings"
+  ADD CONSTRAINT "crossings_source_draft_fk"
+  FOREIGN KEY ("source_draft_id")
+  REFERENCES "crossing_drafts"("id")
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
 
 CREATE UNIQUE INDEX IF NOT EXISTS "crossings_source_draft_unique"
   ON "crossings" ("source_draft_id")
